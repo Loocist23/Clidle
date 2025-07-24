753d3b15aeeeb88aedac2b2de917bbb2b078b9e6
Loocist <anthonyzegnal05@gmail.com>
2025-07-24 15:43:11 +0200

    Refactor ls command to ignore specified files and directories

--- commands/ls.py
+++ commands/ls.py
@@ -1,6 +1,14 @@
 import os
 
+IGNORE_LIST = [
+    "save.json",
+    # Ajoute ici d'autres fichiers ou dossiers à ignorer
+]
+
 def run(args, cli):
     files = os.listdir(cli.home_path)
     for f in files:
-        print(f)
+        if f in IGNORE_LIST:
+            continue
+        if not (os.path.isdir(os.path.join(cli.home_path, f)) and f.startswith("remote_")):
+            print(f)
